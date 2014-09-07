c5d05542711d13b91818238616550f89d7563381
a.alvino <a.alvino@af88ee38-41e2-ecd4-ade0-fbb6e5608b8a>
2014-09-07 20:22:32 +0000

    zip

--- src/it/albertus/cycles/model/Bike.java
+++ src/it/albertus/cycles/model/Bike.java
@@ -15,7 +15,7 @@ public class Bike {
 	private Gearbox gearbox;   // 22-35
 	private Torque torque;     // 36-147
 	
-	public Bike( byte[] inf ) throws InstantiationException, IllegalAccessException {
+	public Bike( byte[] inf ) {
 		this( new Settings( toInt( inf[0], inf[1] ), toInt( inf[2], inf[3] ), toInt( inf[4], inf[5] ), toInt( inf[6], inf[7] ), toInt( inf[8], inf[9] ), toInt( inf[10], inf[11] ), toInt( inf[12], inf[13] ), toInt( inf[14], inf[15] ), toInt( inf[16], inf[17] ), toInt( inf[18], inf[19] ), toInt( inf[20], inf[21] ) ), new Gearbox( toInt( inf[22], inf[23] ), toInt( inf[24], inf[25] ), toInt( inf[26], inf[27] ), toInt( inf[28], inf[29] ), toInt( inf[30], inf[31] ), toInt( inf[32], inf[33] ), toInt( inf[34], inf[35] ) ), new Torque( toShortArray( Arrays.copyOfRange( inf, 36, 148 ) ) ) );
 	}
 	
--- src/it/albertus/cycles/model/Gearbox.java
+++ src/it/albertus/cycles/model/Gearbox.java
@@ -10,33 +10,33 @@ public class Gearbox {
 	protected static final int LENGTH = 14;
 
 	private int neutral; // 22-23
-	private int _1st;    // 24-25
-	private int _2nd;    // 26-27
-	private int _3rd;    // 28-29
-	private int _4th;    // 30-31
-	private int _5th;    // 32-33
-	private int _6th;    // 34-35
+	private int gear1st; // 24-25
+	private int gear2nd; // 26-27
+	private int gear3rd; // 28-29
+	private int gear4th; // 30-31
+	private int gear5th; // 32-33
+	private int gear6th; // 34-35
 	
-	public Gearbox(int neutral, int _1st, int _2nd, int _3rd, int _4th,	int _5th, int _6th) {
+	public Gearbox(int neutral, int gear1st, int gear2nd, int gear3rd, int gear4th, int gear5th, int gear6th) {
 		super();
 		this.neutral = neutral;
-		this._1st = _1st;
-		this._2nd = _2nd;
-		this._3rd = _3rd;
-		this._4th = _4th;
-		this._5th = _5th;
-		this._6th = _6th;
+		this.gear1st = gear1st;
+		this.gear2nd = gear2nd;
+		this.gear3rd = gear3rd;
+		this.gear4th = gear4th;
+		this.gear5th = gear5th;
+		this.gear6th = gear6th;
 	}
 	
 	public List<Byte> toByteList() {
 		List<Byte> byteList = new ArrayList<Byte>( LENGTH );
 		byteList.addAll( ByteUtils.toByteList( neutral ) );
-		byteList.addAll( ByteUtils.toByteList( _1st ) );
-		byteList.addAll( ByteUtils.toByteList( _2nd ) );
-		byteList.addAll( ByteUtils.toByteList( _3rd ) );
-		byteList.addAll( ByteUtils.toByteList( _4th ) );
-		byteList.addAll( ByteUtils.toByteList( _5th ) );
-		byteList.addAll( ByteUtils.toByteList( _6th ) );
+		byteList.addAll( ByteUtils.toByteList( gear1st ) );
+		byteList.addAll( ByteUtils.toByteList( gear2nd ) );
+		byteList.addAll( ByteUtils.toByteList( gear3rd ) );
+		byteList.addAll( ByteUtils.toByteList( gear4th ) );
+		byteList.addAll( ByteUtils.toByteList( gear5th ) );
+		byteList.addAll( ByteUtils.toByteList( gear6th ) );
 		return byteList;
 	}
 	
@@ -46,41 +46,41 @@ public class Gearbox {
 	public void setNeutral(int neutral) {
 		this.neutral = neutral;
 	}
-	public int get_1st() {
-		return _1st;
+	public int getGear1st() {
+		return gear1st;
 	}
-	public void set_1st(int _1st) {
-		this._1st = _1st;
+	public void setGear1st(int gear1st) {
+		this.gear1st = gear1st;
 	}
-	public int get_2nd() {
-		return _2nd;
+	public int getGear2nd() {
+		return gear2nd;
 	}
-	public void set_2nd(int _2nd) {
-		this._2nd = _2nd;
+	public void setGear2nd(int gear2nd) {
+		this.gear2nd = gear2nd;
 	}
-	public int get_3rd() {
-		return _3rd;
+	public int getGear3rd() {
+		return gear3rd;
 	}
-	public void set_3rd(int _3rd) {
-		this._3rd = _3rd;
+	public void setGear3rd(int gear3rd) {
+		this.gear3rd = gear3rd;
 	}
-	public int get_4th() {
-		return _4th;
+	public int getGear4th() {
+		return gear4th;
 	}
-	public void set_4th(int _4th) {
-		this._4th = _4th;
+	public void setGear4th(int gear4th) {
+		this.gear4th = gear4th;
 	}
-	public int get_5th() {
-		return _5th;
+	public int getGear5th() {
+		return gear5th;
 	}
-	public void set_5th(int _5th) {
-		this._5th = _5th;
+	public void setGear5th(int gear5th) {
+		this.gear5th = gear5th;
 	}
-	public int get_6th() {
-		return _6th;
+	public int getGear6th() {
+		return gear6th;
 	}
-	public void set_6th(int _6th) {
-		this._6th = _6th;
+	public void setGear6th(int gear6th) {
+		this.gear6th = gear6th;
 	}
-
+	
 }
