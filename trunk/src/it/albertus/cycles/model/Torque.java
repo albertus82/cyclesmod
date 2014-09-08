3007c5a068810c5bdd0494c5d69bdcde97c4d968
a.alvino <a.alvino@af88ee38-41e2-ecd4-ade0-fbb6e5608b8a>
2014-09-08 20:15:19 +0000

    gearbox array OO

--- trunk/src/it/albertus/cycles/model/Torque.java
+++ trunk/src/it/albertus/cycles/model/Torque.java
@@ -3,30 +3,32 @@ package it.albertus.cycles.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Torque {
+public class Torque extends BikesInfElement {
 	
-	protected static final int LENGTH = 112;
+	protected static final int LENGTH = 106;
 	
-	private short[] curve; // 36-147: curva di coppia (regime massimo considerato: ~14500 RPM).
+	private short[] curve = new short[ LENGTH ]; // 42-147: curva di coppia (regime massimo considerato: ~14500 RPM).
 
 	public Torque(short[] curve) {
-		super();
-		this.setCurve(curve);
+		if ( curve.length > LENGTH ) {
+			throw new IllegalArgumentException( "The torque curve cannot accept more than " + LENGTH + " points. Number of values provided: " + curve.length + '.' );
+		}
+		for ( int i = 0; i < curve.length; i++ ) {
+			this.curve[i] = curve[i];
+		}
 	}
 	
+	@Override
 	public List<Byte> toByteList() {
 		List<Byte> byteList = new ArrayList<Byte>( LENGTH );
-		for ( short pt : curve ) {
-			byteList.add( (byte)pt );
+		for ( short point : curve ) {
+			byteList.add( (byte)point );
 		}
 		return byteList;
 	}
 
 	public short[] getCurve() {
 		return curve;
-	}
-	public void setCurve(short[] curve) {
-		this.curve = curve;
 	}
 
 }
