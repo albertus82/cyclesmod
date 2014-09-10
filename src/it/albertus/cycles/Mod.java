fabf97bad4c051f33f597ef0d954b26479284d0c
a.alvino <a.alvino@af88ee38-41e2-ecd4-ade0-fbb6e5608b8a>
2014-09-10 11:40:12 +0000

    modules

---
+++ src/it/albertus/cycles/Mod.java
@@ -0,0 +1,11 @@
+package it.albertus.cycles;
+
+public class Mod {
+	
+	protected static final String DEFAULT_DESTINATION_PATH = ".";
+	
+	protected static void customize( FileBikesInf bikesInf ) {
+		
+	}
+	
+}
