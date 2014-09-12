8001e74db27d3965850c27fbddb0275d4ed7730d
a.alvino <a.alvino@af88ee38-41e2-ecd4-ade0-fbb6e5608b8a>
2014-09-12 22:06:10 +0000

    external file BIKES.CFG.

--- src/it/albertus/cycles/model/Gearbox.java
+++ src/it/albertus/cycles/model/Gearbox.java
@@ -7,30 +7,30 @@ import java.util.List;
 
 public class Gearbox extends BikesInfElement {
 	
-	protected static final int LENGTH = 20;
+	public static final int LENGTH = 20;
 	
-	private final int[] gearRatios = new int[ LENGTH / 2 ]; // 22-41 (N, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+	private final int[] ratios = new int[ LENGTH / 2 ]; // 22-41 (N, 1, 2, 3, 4, 5, 6, 7, 8, 9)
 	
-	public Gearbox( int[] gearRatios ) {
-		if ( gearRatios.length > LENGTH / 2 ) {
-			throw new IllegalArgumentException( "Il cambio non puo' avere piu' di " + ( LENGTH / 2 ) + " rapporti, incluso folle. Numero rapporti forniti: " + gearRatios.length + '.' );
+	public Gearbox( int[] ratios ) {
+		if ( ratios.length > LENGTH / 2 ) {
+			throw new IllegalArgumentException( "Il cambio non puo' avere piu' di " + ( LENGTH / 2 ) + " rapporti, incluso folle. Numero rapporti forniti: " + ratios.length + '.' );
 		}
-		for ( int i = 0; i < gearRatios.length; i++ ) {
-			this.gearRatios[i] = gearRatios[i];
+		for ( int i = 0; i < ratios.length; i++ ) {
+			this.ratios[i] = ratios[i];
 		}
 	}
 
 	@Override
 	public List<Byte> toByteList() {
 		List<Byte> byteList = new ArrayList<Byte>( LENGTH );
-		for ( int ratio : gearRatios ) {
+		for ( int ratio : ratios ) {
 			byteList.addAll( ByteUtils.toByteList( ratio ) );
 		}
 		return byteList;
 	}
 
-	public int[] getGearRatios() {
-		return gearRatios;
+	public int[] getRatios() {
+		return ratios;
 	}
 	
 }
