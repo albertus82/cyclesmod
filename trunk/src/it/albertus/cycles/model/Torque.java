c334120038d944cfaa011df9f688d738f0e1d98e
a.alvino <a.alvino@af88ee38-41e2-ecd4-ade0-fbb6e5608b8a>
2014-09-18 20:20:01 +0000

    i18n

--- trunk/src/it/albertus/cycles/model/Torque.java
+++ trunk/src/it/albertus/cycles/model/Torque.java
@@ -1,6 +1,6 @@
 package it.albertus.cycles.model;
 
-import it.albertus.cycles.engine.resources.Messages;
+import it.albertus.cycles.resources.Messages;
 
 import java.util.ArrayList;
 import java.util.List;
