3d1feaffe0b9ba9f95bec436dc1747a188ad4e3c
a.alvino <a.alvino@af88ee38-41e2-ecd4-ade0-fbb6e5608b8a>
2014-09-18 19:41:37 +0000

    Improved i18n.

---
+++ trunk/src/it/albertus/cycles/engine/resources/Messages.java
@@ -0,0 +1,19 @@
+package it.albertus.cycles.engine.resources;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Messages {
+	
+	private static final ResourceBundle messages = ResourceBundle.getBundle( Messages.class.getName() );
+	
+	public static String get( final String key, final Object... params ) {
+		List<String> stringParams = new ArrayList<String>( params.length );
+		for ( Object param : params ) {
+			stringParams.add( param.toString() );
+		}
+		return MessageFormat.format( messages.getString( key ), stringParams.toArray() );
+	}
+}
