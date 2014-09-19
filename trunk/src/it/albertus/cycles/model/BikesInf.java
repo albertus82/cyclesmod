c946a8292e9ffcdc4fe22eb6de84ddff8a60434f
a.alvino <a.alvino@af88ee38-41e2-ecd4-ade0-fbb6e5608b8a>
2014-09-19 17:03:27 +0000

    Command line help.

--- trunk/src/it/albertus/cycles/model/BikesInf.java
+++ trunk/src/it/albertus/cycles/model/BikesInf.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public class BikesInf {
 
-	public static final String FILE_NAME = "BIKES.INF";
-	public static final int    FILE_CRC  = 0x28A33682;
-	public static final short  FILE_SIZE = 444;
+	public static final String NAME = "BIKES.INF";
+	public static final int CRC = 0x28A33682;
+	public static final short SIZE = 444;
 	
 	private Bike bike125;
 	private Bike bike250;
@@ -28,12 +28,12 @@ public class BikesInf {
 	 * @return L'array di byte corrispondente al file BIKES.INF.
 	 */
 	public byte[] toByteArray() {
-		List<Byte> byteList = new ArrayList<Byte>( FILE_SIZE );
+		List<Byte> byteList = new ArrayList<Byte>( SIZE );
 		byteList.addAll( bike125.toByteList() );
 		byteList.addAll( bike250.toByteList() );
 		byteList.addAll( bike500.toByteList() );
-		if ( byteList.size() != FILE_SIZE ) {
-			throw new IllegalStateException( Messages.get( "err.wrong.file.size", FILE_NAME, FILE_SIZE, byteList.size() ) );
+		if ( byteList.size() != SIZE ) {
+			throw new IllegalStateException( Messages.get( "err.wrong.file.size", NAME, SIZE, byteList.size() ) );
 		}
 		return ByteUtils.toByteArray( byteList );
 	}
@@ -44,12 +44,14 @@ public class BikesInf {
 	public void setBike125(Bike bike125) {
 		this.bike125 = bike125;
 	}
+	
 	public Bike getBike250() {
 		return bike250;
 	}
 	public void setBike250(Bike bike250) {
 		this.bike250 = bike250;
 	}
+	
 	public Bike getBike500() {
 		return bike500;
 	}
