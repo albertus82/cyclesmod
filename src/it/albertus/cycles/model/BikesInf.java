cebd714043a17356e90799b6afac36d20ce8f3da
Alberto <albertus82@users.noreply.github.com>
2015-05-27 20:40:06 +0200

    Format.

--- src/it/albertus/cycles/model/BikesInf.java
+++ src/it/albertus/cycles/model/BikesInf.java
@@ -20,49 +20,49 @@ import org.slf4j.LoggerFactory;
 
 public class BikesInf {
 
-	private static final Logger log = LoggerFactory.getLogger( BikesInf.class );
-	
+	private static final Logger log = LoggerFactory.getLogger(BikesInf.class);
+
 	public static final String FILE_NAME = "BIKES.INF";
 	public static final int FILE_CRC = 0x28A33682;
 	public static final short FILE_SIZE = 444;
-	
+
 	private final Bike[] bikes = new Bike[3];
-	
-	public BikesInf( final InputStream bikesInfInputStream ) throws IOException {
-		read( bikesInfInputStream );
+
+	public BikesInf(final InputStream bikesInfInputStream) throws IOException {
+		read(bikesInfInputStream);
 	}
-	
-	public BikesInf( String fileName ) throws IOException {
-		read( new BufferedInputStream( new FileInputStream( fileName ) ) );
+
+	public BikesInf(String fileName) throws IOException {
+		read(new BufferedInputStream(new FileInputStream(fileName)));
 	}
-	
-	private void read( final InputStream inf ) throws IOException {
-		byte[] inf125 = new byte[ Bike.LENGTH ];
-		byte[] inf250 = new byte[ Bike.LENGTH ];
-		byte[] inf500 = new byte[ Bike.LENGTH ];
-		
-		if ( inf.read( inf125 ) != Bike.LENGTH || inf.read( inf250 ) != Bike.LENGTH || inf.read( inf500 ) != Bike.LENGTH || inf.read() != -1 ) {
+
+	private void read(final InputStream inf) throws IOException {
+		byte[] inf125 = new byte[Bike.LENGTH];
+		byte[] inf250 = new byte[Bike.LENGTH];
+		byte[] inf500 = new byte[Bike.LENGTH];
+
+		if (inf.read(inf125) != Bike.LENGTH || inf.read(inf250) != Bike.LENGTH || inf.read(inf500) != Bike.LENGTH || inf.read() != -1) {
 			inf.close();
-			throw new IllegalStateException( Resources.get( "err.wrong.file.size" ) );
+			throw new IllegalStateException(Resources.get("err.wrong.file.size"));
 		}
 		inf.close();
-		log.info( Resources.get( "msg.file.read", FILE_NAME ) );
-		
-		bikes[0] = new Bike( Bike.Type.CLASS_125, inf125 );
-		bikes[1] = new Bike( Bike.Type.CLASS_250, inf250 );
-		bikes[2] = new Bike( Bike.Type.CLASS_500, inf500 );
-		log.info( Resources.get( "msg.file.parsed", FILE_NAME ) );
+		log.info(Resources.get("msg.file.read", FILE_NAME));
+
+		bikes[0] = new Bike(Bike.Type.CLASS_125, inf125);
+		bikes[1] = new Bike(Bike.Type.CLASS_250, inf250);
+		bikes[2] = new Bike(Bike.Type.CLASS_500, inf500);
+		log.info(Resources.get("msg.file.parsed", FILE_NAME));
 	}
-	
-	public void write( final String fileName ) throws IOException {
+
+	public void write(final String fileName) throws IOException {
 		byte[] newBikesInf = this.toByteArray();
 		Checksum crc = new CRC32();
-		crc.update( newBikesInf, 0, newBikesInf.length );
-		log.info( Resources.get( "msg.configuration.changed", ( crc.getValue() == FILE_CRC ? ' ' + Resources.get( "msg.not" ) + ' ' : ' ' ), String.format( "%08X", crc.getValue() ) ) );
-	
-		BufferedOutputStream bos = new BufferedOutputStream( new FileOutputStream( fileName ), FILE_SIZE );
-		write( bos, newBikesInf );
-		log.info( Resources.get( "msg.new.file.written.into.path", FILE_NAME, "".equals( fileName ) ? '.' : fileName, String.format( "%08X", crc.getValue() ) ) );
+		crc.update(newBikesInf, 0, newBikesInf.length);
+		log.info(Resources.get("msg.configuration.changed", (crc.getValue() == FILE_CRC ? ' ' + Resources.get("msg.not") + ' ' : ' '), String.format("%08X", crc.getValue())));
+
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileName), FILE_SIZE);
+		write(bos, newBikesInf);
+		log.info(Resources.get("msg.new.file.written.into.path", FILE_NAME, "".equals(fileName) ? '.' : fileName, String.format("%08X", crc.getValue())));
 	}
 
 	private void write(OutputStream outputStream, byte[] bikesInf) throws IOException {
@@ -73,34 +73,35 @@ public class BikesInf {
 		outputStream.flush();
 		outputStream.close();
 	}
-	
+
 	/**
-	 * Ricostruisce il file BIKES.INF a partire dalle 3 configurazioni contenute nell'oggetto (125, 250, 500).
+	 * Ricostruisce il file BIKES.INF a partire dalle 3 configurazioni contenute
+	 * nell'oggetto (125, 250, 500).
 	 * 
 	 * @return L'array di byte corrispondente al file BIKES.INF.
 	 */
 	private byte[] toByteArray() {
-		List<Byte> byteList = new ArrayList<Byte>( FILE_SIZE );
-		for ( Bike bike : bikes ) {
-			byteList.addAll( bike.toByteList() );
+		List<Byte> byteList = new ArrayList<Byte>(FILE_SIZE);
+		for (Bike bike : bikes) {
+			byteList.addAll(bike.toByteList());
 		}
-		if ( byteList.size() != FILE_SIZE ) {
-			throw new IllegalStateException( Resources.get( "err.wrong.file.size.detailed", FILE_NAME, FILE_SIZE, byteList.size() ) );
+		if (byteList.size() != FILE_SIZE) {
+			throw new IllegalStateException(Resources.get("err.wrong.file.size.detailed", FILE_NAME, FILE_SIZE, byteList.size()));
 		}
-		return ByteUtils.toByteArray( byteList );
+		return ByteUtils.toByteArray(byteList);
 	}
 
-	public Bike getBike( int displacement ) {
-		for ( Bike bike : bikes ) {
-			if ( bike.getType().getDisplacement() == displacement ) {
+	public Bike getBike(int displacement) {
+		for (Bike bike : bikes) {
+			if (bike.getType().getDisplacement() == displacement) {
 				return bike;
 			}
 		}
 		return null;
 	}
-	
+
 	public Bike[] getBikes() {
 		return bikes;
 	}
-	
+
 }
--- src/it/albertus/cycles/model/Gearbox.java
+++ src/it/albertus/cycles/model/Gearbox.java
@@ -8,41 +8,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Gearbox extends BikesInfElement {
-	
+
 	public static final int LENGTH = 20;
 	public static final int MIN_VALUE = 0;
-	public static final int MAX_VALUE = 65535; // I valori sono a 16 bit, ma di fatto vengono considerati solo 8 bit (si procede per multipli di 256).
-	
-	private final int[] ratios = new int[ LENGTH / 2 ]; // 22-41 (N, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-	
-	public Gearbox( final int[] ratios ) {
-		if ( ratios.length > LENGTH / 2 ) {
-			throw new IllegalArgumentException( Resources.get( "err.gearbox", LENGTH / 2, ratios.length ) );
+
+	/**
+	 * I valori sono a 16 bit, ma di fatto vengono considerati solo 8 bit (si
+	 * procede per multipli di 256).
+	 */
+	public static final int MAX_VALUE = 65535;
+
+	/** 22-41 (N, 1, 2, 3, 4, 5, 6, 7, 8, 9) */
+	private final int[] ratios = new int[LENGTH / 2];
+
+	public Gearbox(final int[] ratios) {
+		if (ratios.length > LENGTH / 2) {
+			throw new IllegalArgumentException(Resources.get("err.gearbox", LENGTH / 2, ratios.length));
 		}
-		for ( int i = 0; i < ratios.length; i++ ) {
+		for (int i = 0; i < ratios.length; i++) {
 			this.ratios[i] = ratios[i];
 		}
 	}
-	
+
 	@Override
 	public List<Byte> toByteList() {
-		List<Byte> byteList = new ArrayList<Byte>( LENGTH );
-		for ( int ratio : ratios ) {
-			byteList.addAll( ByteUtils.toByteList( ratio ) );
+		List<Byte> byteList = new ArrayList<Byte>(LENGTH);
+		for (int ratio : ratios) {
+			byteList.addAll(ByteUtils.toByteList(ratio));
 		}
 		return byteList;
 	}
-	
-	public static int parse( final String key, final String value ) {
-		long newValue = Long.parseLong( value );
-		if ( newValue < MIN_VALUE || newValue > MAX_VALUE ) {
-			throw new InvalidPropertyException( Resources.get( "err.illegal.value", MIN_VALUE, MAX_VALUE, key, newValue ) );
+
+	public static int parse(final String key, final String value) {
+		long newValue = Long.parseLong(value);
+		if (newValue < MIN_VALUE || newValue > MAX_VALUE) {
+			throw new InvalidPropertyException(Resources.get("err.illegal.value", MIN_VALUE, MAX_VALUE, key, newValue));
 		}
 		return (int) newValue;
 	}
-	
+
 	public int[] getRatios() {
 		return ratios;
 	}
-	
+
 }
