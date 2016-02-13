b5222e0de09e96d81b1323a21b8e5ca894a61a82
Albertus82 <albertus82@gmail.com>
2016-02-13 09:27:06 +0100

    Updated scripts (JAVA_HOME no longer needed in Windows).

--- src/main/scripts/cyclesmod-con.bat
+++ src/main/scripts/cyclesmod-con.bat
@@ -1,1 +1,3 @@
-@"%JAVA_HOME%\bin\java" -classpath "cyclesmod.jar;lib/*" it.albertus.cycles.CyclesModCon %1 %2
+@ECHO OFF
+IF "%JAVA_HOME%" == "" java.exe -classpath "cyclesmod.jar;lib/*" it.albertus.cycles.CyclesModCon %1 %2
+IF NOT "%JAVA_HOME%" == "" "%JAVA_HOME%\bin\java.exe" -classpath "cyclesmod.jar;lib/*" it.albertus.cycles.CyclesModCon %1 %2
--- src/main/scripts/cyclesmod-gui.bat
+++ src/main/scripts/cyclesmod-gui.bat
@@ -1,1 +1,3 @@
-@start "" "%JAVA_HOME%"\bin\javaw -classpath "%~dp0cyclesmod.jar;%~dp0lib/*" it.albertus.cycles.CyclesModGui %1 ""
+@ECHO OFF
+IF "%JAVA_HOME%" == "" START /BELOWNORMAL "" javaw.exe -classpath "%~dp0cyclesmod.jar;%~dp0lib/*" it.albertus.cycles.CyclesModGui %1
+IF NOT "%JAVA_HOME%" == "" START /BELOWNORMAL "" "%JAVA_HOME%\bin\javaw.exe" -classpath "%~dp0cyclesmod.jar;%~dp0lib/*" it.albertus.cycles.CyclesModGui %1
