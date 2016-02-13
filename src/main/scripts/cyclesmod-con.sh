b949ed851dbae50417b8167e152147199794a028
Alberto <albertus82@users.noreply.github.com>
2016-02-13 15:07:06 +0100

    Updated scripts (JAVA_HOME no longer needed in Linux).

--- src/main/scripts/cyclesmod-con.sh
+++ src/main/scripts/cyclesmod-con.sh
@@ -1,1 +1,4 @@
-"$JAVA_HOME/bin/java" -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesModCon $1 $2
+if [ "$JAVA_HOME" != "" ]
+then "$JAVA_HOME/bin/java" -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesModCon $1 $2
+else java -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesModCon $1 $2
+fi
