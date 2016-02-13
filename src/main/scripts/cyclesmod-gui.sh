4fb912410f796f43c58ff80c62fd431e990b3d0c
Albertus82 <albertus82@gmail.com>
2016-02-13 15:07:06 +0100

    Updated scripts (JAVA_HOME no longer needed in Linux).

--- src/main/scripts/cyclesmod-gui.sh
+++ src/main/scripts/cyclesmod-gui.sh
@@ -1,1 +1,4 @@
-"$JAVA_HOME/bin/java" -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesModGui $1
+if [ "$JAVA_HOME" != "" ]
+then "$JAVA_HOME/bin/java" -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesModGui $1
+else java -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesModGui $1
+fi
