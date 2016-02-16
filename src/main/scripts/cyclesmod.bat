29f150cfd2d28407616df91995a1b7b6afdf889e
Albertus82 <albertus82@gmail.com>
2016-02-16 20:33:53 +0100

    Shell scripts.

--- src/main/scripts/cyclesmod.bat
+++ src/main/scripts/cyclesmod.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
-IF "%1" == "/c" GOTO CON
-IF "%1" == "/C" GOTO CON
 IF "%1" == "/?" GOTO HLP
+IF NOT "%3" == "" GOTO ERR
+IF /I "%1" == "/C" GOTO CON
 GOTO GUI
 :CON
 IF "%JAVA_HOME%" == "" java.exe -classpath "%~dp0cyclesmod.jar;%~dp0lib/*" it.albertus.cycles.CyclesModCon %2
@@ -12,16 +12,17 @@ IF "%JAVA_HOME%" == "" START "" javaw.exe -classpath "%~dp0cyclesmod.jar;%~dp0li
 IF NOT "%JAVA_HOME%" == "" START "" "%JAVA_HOME%\bin\javaw.exe" -classpath "%~dp0cyclesmod.jar;%~dp0lib/*" it.albertus.cycles.CyclesModGui %1
 GOTO END
 :ERR
-ECHO.CyclesMod: unrecognized option '%1'
+ECHO.CyclesMod: too many parameters - %3
 ECHO.Try 'CYCLESMOD /?' for more information.
 GOTO END
 :HLP
-ECHO.Launches CyclesMod application.
+ECHO.Start CyclesMod application.
 ECHO.
-ECHO.Usage: CYCLESMOD [[sourcefile] | /C [destination]]
+ECHO.Usage: CYCLESMOD [[sourcefile] ^| /C [destination]] [/?]
 ECHO.
-ECHO   sourcefile     CFG or INF file to load.
-ECHO   /C             Runs in console mode.
-ECHO   destination    Destination path for generated BIKES.CFG and BIKES.INF.
-ECHO.
+ECHO.  sourcefile     CFG or INF file to load (GUI mode).
+ECHO.  /C             Start in console mode.
+ECHO.  destination    Path for generated BIKES.CFG and BIKES.INF (console mode).
+ECHO.  /?             Display this help and exit.
+GOTO END
 :END
--- src/main/scripts/cyclesmod.sh
+++ src/main/scripts/cyclesmod.sh
@@ -1,17 +1,22 @@
-if [ "$1" = "-c" ] || [ "$1" = "-C" ]
+if [ "$1" = "--help" ]
+  then
+  echo "Start CyclesMod application."
+  echo
+  echo "Usage: cyclesmod.sh [[sourcefile] | -c [destination]] [--help]"
+  echo
+  echo "  sourcefile     CFG or INF file to load (GUI mode)"
+  echo "  -c             start in console mode"
+  echo "  destination    path for generated BIKES.CFG and BIKES.INF (console mode)"
+  echo "  --help         display this help and exit"
+elif [ "$3" != "" ]
+  then
+  echo "cyclesmod: too many parameters - $3"
+  echo "Try 'cyclesmod.sh --help' for more information."
+elif [ "$1" = "-c" ] || [ "$1" = "-C" ]
   then if [ "$JAVA_HOME" != "" ]
   then "$JAVA_HOME/bin/java" -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesModCon $2
   else java -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesModCon $2
   fi
-elif [ "$1" = "--help" ]
-  then
-  echo "Launches CyclesMod application."
-  echo
-  echo "Usage: cyclesmod.sh [[sourcefile] | -c [destination]]"
-  echo
-  echo "  sourcefile     CFG or INF file to load."
-  echo "  -c             Runs in console mode."
-  echo "  destination    Destination path for generated BIKES.CFG and BIKES.INF."
 else
   if [ "$JAVA_HOME" != "" ]
   then "$JAVA_HOME/bin/java" -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesModGui $1
