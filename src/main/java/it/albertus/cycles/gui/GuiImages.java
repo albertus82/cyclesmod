33790d7b56094b9f77bed192af9fe245205f976e
Alberto <albertus82@users.noreply.github.com>
2016-02-20 14:14:00 +0100

    Icons.

---
+++ src/main/java/it/albertus/cycles/gui/GuiImages.java
@@ -0,0 +1,31 @@
+package it.albertus.cycles.gui;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
+import org.eclipse.swt.widgets.Display;
+
+public class GuiImages {
+
+	/* Icona principale dell'applicazione (in vari formati) */
+	public static final Image[] ICONS_BIKE = loadIcons("bike.ico");
+
+	private static Image[] loadIcons(final String fileName) {
+		final InputStream is = GuiImages.class.getResourceAsStream(fileName);
+		final ImageData[] images = new ImageLoader().load(is);
+		try {
+			is.close();
+		}
+		catch (IOException ioe) {}
+		final Image[] icons = new Image[images.length];
+		int i = 0;
+		for (final ImageData id : images) {
+			icons[i++] = new Image(Display.getCurrent(), id);
+		}
+		return icons;
+	}
+
+}
