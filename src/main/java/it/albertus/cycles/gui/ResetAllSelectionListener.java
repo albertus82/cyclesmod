41d8ac7501fff093f4a2dcbced0cb52ed2298d58
Albertus82 <albertus82@gmail.com>
2016-03-03 21:28:56 +0100

    Reset & Reset all.

--- src/main/java/it/albertus/cycles/gui/ResetAllSelectionListener.java
+++ src/main/java/it/albertus/cycles/gui/ResetAllSelectionListener.java
@@ -1,6 +1,7 @@
 package it.albertus.cycles.gui;
 
-import it.albertus.cycles.model.Bike;
+import it.albertus.cycles.data.BikesZip;
+import it.albertus.cycles.model.BikesInf;
 import it.albertus.cycles.resources.Resources;
 import it.albertus.util.ExceptionUtils;
 
@@ -9,32 +10,28 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.MessageBox;
 
-public class ResetSelectionListener extends SelectionAdapter {
+public class ResetAllSelectionListener extends SelectionAdapter {
 
 	private final CyclesModGui gui;
 
-	public ResetSelectionListener(final CyclesModGui gui) {
+	public ResetAllSelectionListener(final CyclesModGui gui) {
 		this.gui = gui;
 	}
 
 	@Override
 	public void widgetSelected(SelectionEvent event) {
-		int choose = SWT.YES;
-		if (gui.getBikesInf() != null) {
-			MessageBox messageBox = new MessageBox(gui.getShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO);
-			messageBox.setText(Resources.get("msg.warning"));
-			messageBox.setMessage(Resources.get("msg.reset.overwrite"));
-			choose = messageBox.open();
-		}
+		MessageBox messageBox = new MessageBox(gui.getShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO);
+		messageBox.setText(Resources.get("msg.warning"));
+		messageBox.setMessage(Resources.get("msg.reset.overwrite.all"));
+		int choose = messageBox.open();
 		if (choose == SWT.YES) {
 			try {
-				gui.updateModelValues(true);
-				gui.getBikesInf().reset(Bike.Type.values()[gui.getTabFolder().getSelectionIndex()]);
+				gui.setBikesInf(new BikesInf(new BikesZip().getInputStream()));
 				gui.updateFormValues();
 			}
 			catch (Exception e) {
 				System.err.println(ExceptionUtils.getLogMessage(e));
-				MessageBox messageBox = new MessageBox(gui.getShell(), SWT.ICON_ERROR);
+				messageBox = new MessageBox(gui.getShell(), SWT.ICON_ERROR);
 				messageBox.setText(Resources.get("msg.warning"));
 				messageBox.setMessage(Resources.get("err.reset", ExceptionUtils.getUIMessage(e)));
 				messageBox.open();
