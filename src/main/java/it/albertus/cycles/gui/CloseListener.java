5476d85b4b11a6c62f530d24389d19e57873fbd9
Albertus82 <albertus82@gmail.com>
2016-03-03 22:30:01 +0100

    CloseListener.

--- src/main/java/it/albertus/cycles/gui/CloseListener.java
+++ src/main/java/it/albertus/cycles/gui/CloseListener.java
@@ -3,24 +3,37 @@ package it.albertus.cycles.gui;
 import it.albertus.cycles.resources.Resources;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.MessageBox;
 
-public class CloseButtonListener implements Listener {
+public class CloseListener extends SelectionAdapter implements Listener {
 
 	private final Gui gui;
 
-	public CloseButtonListener(Gui gui) {
+	public CloseListener(Gui gui) {
 		this.gui = gui;
 	}
 
 	@Override
+	public void widgetSelected(SelectionEvent e) {
+		if (confirmClose()) {
+			gui.getShell().dispose();
+		}
+	}
+
+	@Override
 	public void handleEvent(Event event) {
+		event.doit = confirmClose();
+	}
+
+	private boolean confirmClose() {
 		final MessageBox messageBox = new MessageBox(gui.getShell(), SWT.YES | SWT.NO | SWT.ICON_QUESTION);
 		messageBox.setText(Resources.get("msg.confirm.close.text"));
 		messageBox.setMessage(Resources.get("msg.confirm.close.message"));
-		event.doit = messageBox.open() == SWT.YES;
+		return messageBox.open() == SWT.YES;
 	}
 
 }
