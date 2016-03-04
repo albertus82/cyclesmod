4014513d8309e30a18c0a11eb6696e3bedfb2a48
Albertus82 <albertus82@gmail.com>
2016-03-04 17:52:15 +0100

    About dialog.

--- src/main/java/it/albertus/cycles/gui/AboutDialog.java
+++ src/main/java/it/albertus/cycles/gui/AboutDialog.java
@@ -46,33 +46,31 @@ public class AboutDialog extends Dialog {
 		shell.setLayout(new GridLayout(2, false));
 
 		final Label icon = new Label(shell, SWT.NONE);
-		icon.setImage(shell.getDisplay().getSystemImage(SWT.ICON_INFORMATION));
-		GridData gridData = new GridData();
-		gridData.verticalSpan = 3;
+		icon.setImage(Images.ICONS_TOOLS[5]);
+		GridData gridData = new GridData(GridData.CENTER, GridData.CENTER, false, false, 0, 3);
 		icon.setLayoutData(gridData);
 
 		final Label info = new Label(shell, SWT.NONE);
 		info.setText(this.message);
-		gridData = new GridData(SWT.LEFT, SWT.CENTER, false, false);
+		gridData = new GridData(SWT.LEFT, SWT.CENTER, false, true);
 		info.setLayoutData(gridData);
 
 		final Link linkProject = new Link(shell, SWT.NONE);
 		linkProject.setText("<a href=\"" + getLinkProject() + "\">" + getLinkProject() + "</a>");
-		gridData = new GridData(SWT.LEFT, SWT.CENTER, false, false);
+		gridData = new GridData(SWT.LEFT, SWT.CENTER, false, true);
 		linkProject.setLayoutData(gridData);
 		linkProject.addSelectionListener(new LinkSelectionListener());
 		
 		final Link linkIcon = new Link(shell, SWT.NONE);
 		String url = getLinkIcon().startsWith("http") ? getLinkIcon() : "http://" + getLinkIcon();
 		linkIcon.setText(Resources.get("msg.info.icon") + " <a href=\"" + url + "\">" + getLinkIcon() + "</a>");
-		gridData = new GridData(SWT.LEFT, SWT.CENTER, false, false);
+		gridData = new GridData(SWT.LEFT, SWT.CENTER, false, true);
 		linkIcon.setLayoutData(gridData);
 		linkIcon.addSelectionListener(new LinkSelectionListener());
 
 		Button okButton = new Button(shell, SWT.PUSH);
 		okButton.setText(Resources.get("lbl.button.ok"));
-		gridData = new GridData(SWT.CENTER, SWT.CENTER, true, false);
-		gridData.horizontalSpan = 2;
+		gridData = new GridData(SWT.CENTER, SWT.CENTER, true, false, 2, 0);
 		gridData.minimumWidth = 64;
 		okButton.setLayoutData(gridData);
 		okButton.addSelectionListener(new SelectionAdapter() {
