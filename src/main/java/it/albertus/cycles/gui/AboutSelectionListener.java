d2e6d2454ae339cc0bfef26e2b2218f5f7e32369
Alberto <albertus82@users.noreply.github.com>
2016-03-04 15:57:38 +0100

    About.

--- src/main/java/it/albertus/cycles/gui/AboutSelectionListener.java
+++ src/main/java/it/albertus/cycles/gui/AboutSelectionListener.java
@@ -1,13 +1,10 @@
 package it.albertus.cycles.gui;
 
 import it.albertus.cycles.resources.Resources;
-import it.albertus.util.NewLine;
 import it.albertus.util.Version;
 
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.widgets.MessageBox;
 
 public class AboutSelectionListener extends SelectionAdapter {
 
@@ -19,10 +16,12 @@ public class AboutSelectionListener extends SelectionAdapter {
 
 	@Override
 	public void widgetSelected(SelectionEvent event) {
-		MessageBox messageBox = new MessageBox(gui.getShell(), SWT.ICON_INFORMATION);
-		messageBox.setText(Resources.get("msg.info.title"));
-		messageBox.setMessage(Resources.get("msg.info.body", Version.getInstance().getNumber(), Version.getInstance().getDate()) + NewLine.SYSTEM_LINE_SEPARATOR + Resources.get("msg.info.site") + NewLine.SYSTEM_LINE_SEPARATOR + Resources.get("msg.info.icon"));
-		messageBox.open();
+		final AboutDialog aboutDialog = new AboutDialog(gui.getShell());
+		aboutDialog.setText(Resources.get("msg.info.title"));
+		aboutDialog.setMessage(Resources.get("msg.info.body", Version.getInstance().getNumber(), Version.getInstance().getDate()));
+		aboutDialog.setLinkProject(Resources.get("msg.info.site"));
+		aboutDialog.setLinkIcon(Resources.get("msg.info.icon.site"));
+		aboutDialog.open();
 	}
 
 }
