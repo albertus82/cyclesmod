9538e6d03dfbaab96dbd5dd9a2b9fc6229a597c6
Alberto <albertus82@users.noreply.github.com>
2016-03-04 14:29:56 +0100

    Mac OS X.

--- src/main/scripts/cyclesmod.sh
+++ src/main/scripts/cyclesmod.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 if [ "$1" = "-c" ] || [ "$1" = "-C" ] || [ "$1" = "--help" ] || [ "$1" = "--HELP" ]
   then if [ "$JAVA_HOME" != "" ]
   then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -jar cyclesmod.jar $1 $2 $3
