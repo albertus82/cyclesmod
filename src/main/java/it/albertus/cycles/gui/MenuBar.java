0d1484c70c648f5a1c9026b19ea58854de339855
Albertus82 <albertus82@gmail.com>
2016-03-05 17:27:52 +0100

    Accelerators.

--- src/main/java/it/albertus/cycles/gui/MenuBar.java
+++ src/main/java/it/albertus/cycles/gui/MenuBar.java
@@ -50,12 +50,12 @@ public class MenuBar {
 		fileMenuHeader.setMenu(fileMenu);
 
 		fileOpenMenuItem = new MenuItem(fileMenu, SWT.PUSH);
-		fileOpenMenuItem.setText(Resources.get("lbl.menu.item.open") + GuiUtils.getMod1KeyLabel() + Character.toUpperCase(GuiUtils.KEY_OPEN));
+		fileOpenMenuItem.setText(Resources.get("lbl.menu.item.open") + GuiUtils.getMod1ShortcutLabel(GuiUtils.KEY_OPEN));
 		fileOpenMenuItem.addSelectionListener(new OpenSelectionListener(gui));
 		fileOpenMenuItem.setAccelerator(SWT.MOD1 | GuiUtils.KEY_OPEN);
 
 		fileSaveMenuItem = new MenuItem(fileMenu, SWT.PUSH);
-		fileSaveMenuItem.setText(Resources.get("lbl.menu.item.saveas") + GuiUtils.getMod1KeyLabel() + Character.toUpperCase(GuiUtils.KEY_SAVE));
+		fileSaveMenuItem.setText(Resources.get("lbl.menu.item.saveas") + GuiUtils.getMod1ShortcutLabel(GuiUtils.KEY_SAVE));
 		fileSaveMenuItem.addSelectionListener(new SaveSelectionListener(gui));
 		fileSaveMenuItem.setAccelerator(SWT.MOD1 | GuiUtils.KEY_SAVE);
 
@@ -73,17 +73,17 @@ public class MenuBar {
 		editMenuHeader.addArmListener(new EditArmListener(gui));
 
 		editCutMenuItem = new MenuItem(editMenu, SWT.PUSH);
-		editCutMenuItem.setText(Resources.get("lbl.menu.item.cut") + GuiUtils.getMod1KeyLabel() + Character.toUpperCase(GuiUtils.KEY_CUT));
+		editCutMenuItem.setText(Resources.get("lbl.menu.item.cut") + GuiUtils.getMod1ShortcutLabel(GuiUtils.KEY_CUT));
 		editCutMenuItem.addSelectionListener(new CutSelectionListener(gui));
 		editCutMenuItem.setAccelerator(SWT.MOD1 | GuiUtils.KEY_CUT);
 
 		editCopyMenuItem = new MenuItem(editMenu, SWT.PUSH);
-		editCopyMenuItem.setText(Resources.get("lbl.menu.item.copy") + GuiUtils.getMod1KeyLabel() + Character.toUpperCase(GuiUtils.KEY_COPY));
+		editCopyMenuItem.setText(Resources.get("lbl.menu.item.copy") + GuiUtils.getMod1ShortcutLabel(GuiUtils.KEY_COPY));
 		editCopyMenuItem.addSelectionListener(new CopySelectionListener(gui));
 		editCopyMenuItem.setAccelerator(SWT.MOD1 | GuiUtils.KEY_COPY);
 
 		editPasteMenuItem = new MenuItem(editMenu, SWT.PUSH);
-		editPasteMenuItem.setText(Resources.get("lbl.menu.item.paste") + GuiUtils.getMod1KeyLabel() + Character.toUpperCase(GuiUtils.KEY_PASTE));
+		editPasteMenuItem.setText(Resources.get("lbl.menu.item.paste") + GuiUtils.getMod1ShortcutLabel(GuiUtils.KEY_PASTE));
 		editPasteMenuItem.addSelectionListener(new PasteSelectionListener(gui));
 		editPasteMenuItem.setAccelerator(SWT.MOD1 | GuiUtils.KEY_PASTE);
 
