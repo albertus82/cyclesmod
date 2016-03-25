414b675c93031d43509c5cb64e4be31207e3bf28
Alberto <albertus82@users.noreply.github.com>
2016-03-25 12:42:02 +0100

    Resources.

--- src/main/java/it/albertus/cycles/resources/Resources.java
+++ src/main/java/it/albertus/cycles/resources/Resources.java
@@ -3,20 +3,29 @@ package it.albertus.cycles.resources;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
-
-import org.apache.commons.lang.StringUtils;
 
 public class Resources {
 
-	private static final ResourceBundle resources = ResourceBundle.getBundle(Resources.class.getName().toLowerCase());
+	private static final String BASE_NAME = Resources.class.getName().toLowerCase();
+
+	private static ResourceBundle resources = ResourceBundle.getBundle(BASE_NAME, ResourceBundle.Control.getNoFallbackControl(ResourceBundle.Control.FORMAT_PROPERTIES));
+
+	/** Aggiorna la lingua in cui vengono mostrati i messaggi. */
+	public static void setLanguage(final String language) {
+		if (language != null) {
+			resources = ResourceBundle.getBundle(BASE_NAME, new Locale(language), ResourceBundle.Control.getNoFallbackControl(ResourceBundle.Control.FORMAT_PROPERTIES));
+		}
+	}
 
 	public static String get(final String key, final Object... params) {
-		List<String> stringParams = new ArrayList<String>(params.length);
-		for (Object param : params) {
+		final List<String> stringParams = new ArrayList<String>(params.length);
+		for (final Object param : params) {
 			stringParams.add(param != null ? param.toString() : "");
 		}
-		return StringUtils.trimToEmpty(MessageFormat.format(resources.getString(key), stringParams.toArray()));
+		final String message = MessageFormat.format(resources.getString(key), stringParams.toArray());
+		return message != null ? message.trim() : "";
 	}
 
 }
