ef496ad6a718282b1d79ab45faa49b130ef0ecff
Alberto <albertus82@users.noreply.github.com>
2016-06-04 14:44:11 +0200

    Linux shell script (classpath).

--- src/main/scripts/cyclesmod.sh
+++ src/main/scripts/cyclesmod.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ "$1" = "-c" ] || [ "$1" = "-C" ] || [ "$1" = "--help" ] || [ "$1" = "--HELP" ]
   then if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -jar cyclesmod.jar $1 $2 $3
-  else java -Xms4m -Xmx8m -jar cyclesmod.jar $1 $2 $3
+  then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesMod $1 $2 $3
+  else java -Xms4m -Xmx8m -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesMod $1 $2 $3
   fi
 else
   if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms8m -Xmx16m -jar cyclesmod.jar $1
-  else java -Xms8m -Xmx16m -jar cyclesmod.jar $1
+  then "$JAVA_HOME/bin/java" -Xms8m -Xmx16m -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesMod $1
+  else java -Xms8m -Xmx16m -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesMod $1
   fi
 fi
