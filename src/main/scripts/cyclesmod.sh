6436580e32b00c7ebb3eafe1b3b8a1029497ccfd
Alberto <albertus82@users.noreply.github.com>
2016-06-19 21:18:32 +0200

    Improved shell script.

--- src/main/scripts/cyclesmod.sh
+++ src/main/scripts/cyclesmod.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
+PRG="$0"
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+PRGDIR=`dirname "$PRG"`
 if [ "$1" = "-c" ] || [ "$1" = "-C" ] || [ "$1" = "--help" ] || [ "$1" = "--HELP" ]
   then if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesMod $1 $2 $3
-  else java -Xms4m -Xmx8m -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesMod $1 $2 $3
+  then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -classpath "$PRGDIR/cyclesmod.jar:$PRGDIR/lib/*" it.albertus.cycles.CyclesMod $1 $2 $3
+  else java -Xms4m -Xmx8m -classpath "$PRGDIR/cyclesmod.jar:$PRGDIR/lib/*" it.albertus.cycles.CyclesMod $1 $2 $3
   fi
 else
   if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms8m -Xmx16m -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesMod $1
-  else java -Xms8m -Xmx16m -classpath "cyclesmod.jar:lib/*" it.albertus.cycles.CyclesMod $1
+  then "$JAVA_HOME/bin/java" -Xms8m -Xmx16m -classpath "$PRGDIR/cyclesmod.jar:$PRGDIR/lib/*" it.albertus.cycles.CyclesMod $1
+  else java -Xms8m -Xmx16m -classpath "$PRGDIR/cyclesmod.jar:$PRGDIR/lib/*" it.albertus.cycles.CyclesMod $1
   fi
 fi
