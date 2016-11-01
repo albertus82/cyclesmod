eeaffb1e7567325cf35f884aa42ab9b9dcb7b10a
Alberto <albertus82@users.noreply.github.com>
2016-11-01 12:54:19 +0100

    OS X menu integration.

--- src/main/java/it/albertus/cycles/gui/listener/AboutListener.java
+++ src/main/java/it/albertus/cycles/gui/listener/AboutListener.java
@@ -1,29 +1,36 @@
 package it.albertus.cycles.gui.listener;
 
+import org.eclipse.jface.window.IShellProvider;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+
 import it.albertus.cycles.gui.AboutDialog;
 import it.albertus.cycles.resources.Messages;
 import it.albertus.util.Version;
 
-import org.eclipse.jface.window.IShellProvider;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-
-public class AboutSelectionListener extends SelectionAdapter {
+public class AboutListener extends SelectionAdapter implements Listener {
 
 	private final IShellProvider gui;
 
-	public AboutSelectionListener(IShellProvider gui) {
+	public AboutListener(final IShellProvider gui) {
 		this.gui = gui;
 	}
 
 	@Override
-	public void widgetSelected(SelectionEvent event) {
+	public void widgetSelected(final SelectionEvent se) {
 		final AboutDialog aboutDialog = new AboutDialog(gui.getShell());
 		aboutDialog.setText(Messages.get("msg.info.title"));
 		aboutDialog.setMessage(Messages.get("msg.info.body", Version.getInstance().getNumber(), Version.getInstance().getDate()));
 		aboutDialog.setApplicationUrl(Messages.get("msg.info.site"));
 		aboutDialog.setIconUrl(Messages.get("msg.info.icon.site"));
 		aboutDialog.open();
+	}
+
+	@Override
+	public void handleEvent(final Event event) {
+		widgetSelected(null);
 	}
 
 }
