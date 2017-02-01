e3a726ba6698694f4d97ea6632b5b9832ab63653
Alberto <Albertus82@users.noreply.github.com>
2017-02-01 20:31:55 +0100

    CloseListener.

--- src/main/java/it/albertus/cycles/gui/listener/CloseListener.java
+++ src/main/java/it/albertus/cycles/gui/listener/CloseListener.java
@@ -4,6 +4,7 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.events.ShellEvent;
 import org.eclipse.swt.events.ShellListener;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 
@@ -12,43 +13,51 @@ import it.albertus.cycles.resources.Messages;
 
 public class CloseListener extends AskForSavingListener implements ShellListener, SelectionListener, Listener {
 
-	private static final String MSG_KEY_CONFIRM_CLOSE_MESSAGE = "msg.confirm.close.message";
-	private static final String MSG_KEY_CONFIRM_CLOSE_TEXT = "msg.confirm.close.text";
-
 	public CloseListener(final CyclesModGui gui) {
 		super(gui);
 	}
 
-	/** Comando di chiusura da men&ugrave;. */
-	@Override
-	public void widgetSelected(final SelectionEvent event) {
-		if (askForSaving(Messages.get(MSG_KEY_CONFIRM_CLOSE_TEXT), Messages.get(MSG_KEY_CONFIRM_CLOSE_MESSAGE))) {
-			gui.getShell().dispose();
-			event.display.dispose(); // Fix close not working on Windows 10 when iconified
-		}
-		else {
-			event.doit = false;
+	private boolean canClose() {
+		return askForSaving(Messages.get("msg.confirm.close.text"), Messages.get("msg.confirm.close.message"));
+	}
+
+	private void disposeShellAndDisplay() {
+		gui.getShell().dispose();
+		final Display display = Display.getCurrent();
+		if (display != null) {
+			display.dispose(); // Fix close not working on Windows 10 when iconified
 		}
 	}
 
+	/* OS X Menu */
 	@Override
 	public void handleEvent(final Event event) {
-		if (askForSaving(Messages.get(MSG_KEY_CONFIRM_CLOSE_TEXT), Messages.get(MSG_KEY_CONFIRM_CLOSE_MESSAGE))) {
-			gui.getShell().dispose();
-			event.display.dispose(); // Fix close not working on Windows 10 when iconified
+		if (canClose()) {
+			disposeShellAndDisplay();
 		}
-		else {
+		else if (event != null) {
 			event.doit = false;
 		}
 	}
 
+	/* Menu */
+	@Override
+	public void widgetSelected(final SelectionEvent event) {
+		if (canClose()) {
+			disposeShellAndDisplay();
+		}
+		else if (event != null) {
+			event.doit = false;
+		}
+	}
+
+	/* Shell close command */
 	@Override
 	public void shellClosed(final ShellEvent event) {
-		if (askForSaving(Messages.get(MSG_KEY_CONFIRM_CLOSE_TEXT), Messages.get(MSG_KEY_CONFIRM_CLOSE_MESSAGE))) {
-			gui.getShell().dispose();
-			event.display.dispose(); // Fix close not working on Windows 10 when iconified
+		if (canClose()) {
+			disposeShellAndDisplay();
 		}
-		else {
+		else if (event != null) {
 			event.doit = false;
 		}
 	}
