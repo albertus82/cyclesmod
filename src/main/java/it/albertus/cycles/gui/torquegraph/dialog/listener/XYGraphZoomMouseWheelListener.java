a9f838d9e5cf00b3229a446a824b89a4744733ec
Alberto <albertus82@gmail.com>
2017-07-01 20:26:26 +0200

    XYGraphZoomMouseWheelListener: undo/redo.

--- src/main/java/it/albertus/cycles/gui/torquegraph/dialog/listener/XYGraphZoomMouseWheelListener.java
+++ src/main/java/it/albertus/cycles/gui/torquegraph/dialog/listener/XYGraphZoomMouseWheelListener.java
@@ -2,9 +2,12 @@ package it.albertus.cycles.gui.torquegraph.dialog.listener;
 
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.TreeSearch;
+import org.eclipse.nebula.visualization.internal.xygraph.undo.SaveStateCommand;
+import org.eclipse.nebula.visualization.internal.xygraph.undo.ZoomCommand;
 import org.eclipse.nebula.visualization.xygraph.figures.Axis;
 import org.eclipse.nebula.visualization.xygraph.figures.IXYGraph;
 import org.eclipse.nebula.visualization.xygraph.figures.PlotArea;
+import org.eclipse.nebula.visualization.xygraph.figures.ZoomType;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseWheelListener;
 
@@ -29,18 +32,33 @@ public class XYGraphZoomMouseWheelListener implements MouseWheelListener {
 
 			@Override
 			public boolean accept(final IFigure figure) {
-				return figure instanceof Axis || figure instanceof PlotArea;
+				return figure instanceof PlotArea || figure instanceof Axis;
 			}
 		});
-		if (figureUnderMouse instanceof Axis) {
-			final Axis axis = ((Axis) figureUnderMouse);
-			final double valuePosition = axis.getPositionValue(axis.isHorizontal() ? e.x : e.y, false);
-			axis.zoomInOut(valuePosition, e.count * ZOOM_RATIO / DIVISOR);
-		}
-		else if (figureUnderMouse instanceof PlotArea) {
+
+		SaveStateCommand command = null;
+
+		if (figureUnderMouse instanceof PlotArea) {
 			final PlotArea plotArea = (PlotArea) figureUnderMouse;
+			command = createCommand(e);
 			plotArea.zoomInOut(true, true, e.x, e.y, e.count * ZOOM_RATIO / DIVISOR);
 		}
+		else if (figureUnderMouse instanceof Axis) {
+			final Axis axis = ((Axis) figureUnderMouse);
+			final double valuePosition = axis.getPositionValue(axis.isHorizontal() ? e.x : e.y, false);
+			command = createCommand(e);
+			axis.zoomInOut(valuePosition, e.count * ZOOM_RATIO / DIVISOR);
+		}
+
+		if (command != null) {
+			command.saveState();
+			xyGraph.getOperationsManager().addCommand(command);
+		}
+	}
+
+	private SaveStateCommand createCommand(final MouseEvent e) {
+		final ZoomType zoomType = e.count > 0 ? ZoomType.ZOOM_IN : ZoomType.ZOOM_OUT;
+		return new ZoomCommand(zoomType.getDescription(), xyGraph.getXAxisList(), xyGraph.getYAxisList());
 	}
 
 }
