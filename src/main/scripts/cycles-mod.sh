fdd2866c14fef9c2f3ff8bb53aa361077f5a8f98
Alberto <albertus82@users.noreply.github.com>
2018-12-20 22:53:27 +0100

    Build.

--- src/main/scripts/cycles-mod.sh
+++ src/main/scripts/cycles-mod.sh
@@ -12,12 +12,12 @@ done
 PRGDIR=`dirname "$PRG"`
 if [ "$1" = "-c" ] || [ "$1" = "-C" ] || [ "$1" = "--help" ] || [ "$1" = "--HELP" ]
   then if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx16m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" it.albertus.cyclesmod.CyclesMod $1 $2 $3
-  else java -Xms4m -Xmx16m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" it.albertus.cyclesmod.CyclesMod $1 $2 $3
+  then "$JAVA_HOME/bin/java" -Xms4m -Xmx16m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" @mainClass@ $1 $2 $3
+  else java -Xms4m -Xmx16m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" @mainClass@ $1 $2 $3
   fi
 else
   if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -DSWT_GTK3=0 -Xms@vm.initialHeapSize@m -Xmx@vm.maxHeapSize@m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" it.albertus.cyclesmod.CyclesMod $1
-  else java -DSWT_GTK3=0 -Xms@vm.initialHeapSize@m -Xmx@vm.maxHeapSize@m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" it.albertus.cyclesmod.CyclesMod $1
+  then "$JAVA_HOME/bin/java" -DSWT_GTK3=0 -Xms@vm.initialHeapSize@m -Xmx@vm.maxHeapSize@m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" @mainClass@ $1
+  else java -DSWT_GTK3=0 -Xms@vm.initialHeapSize@m -Xmx@vm.maxHeapSize@m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" @mainClass@ $1
   fi
 fi
