85abfa1ae9bc00d918a3461a686a0628da55ac25
Alberto <albertus82@users.noreply.github.com>
2021-04-24 19:28:26 +0200

    Refactoring.

--- src/main/java/it/albertus/cyclesmod/common/engine/CyclesModEngine.java
+++ src/main/java/it/albertus/cyclesmod/common/engine/CyclesModEngine.java
@@ -23,20 +23,20 @@ public abstract class CyclesModEngine implements NumeralSystemProvider {
 		return isNumeric(value, numeralSystem.getRadix());
 	}
 
-	protected boolean applyProperty(final String key, final String value, final boolean lenient) throws UnknownPropertyException, InvalidNumberException, ValueOutOfRangeException {
+	protected boolean applyProperty(final String propertyName, final String value, final boolean lenient) throws UnknownPropertyException, InvalidNumberException, ValueOutOfRangeException {
 		boolean applied = false;
 		try {
-			if (isSettingsProperty(key)) {
-				applied = applySettingProperty(key, value);
+			if (isSettingsProperty(propertyName)) {
+				applied = applySettingProperty(propertyName, value);
 			}
-			else if (isGearboxProperty(key)) {
-				applied = applyGearboxProperty(key, value);
+			else if (isGearboxProperty(propertyName)) {
+				applied = applyGearboxProperty(propertyName, value);
 			}
-			else if (isPowerProperty(key)) {
-				applied = applyPowerProperty(key, value);
+			else if (isPowerProperty(propertyName)) {
+				applied = applyPowerProperty(propertyName, value);
 			}
 			else {
-				throw new UnknownPropertyException(key);
+				throw new UnknownPropertyException(propertyName);
 			}
 		}
 		catch (final UnknownPropertyException | InvalidNumberException | ValueOutOfRangeException e) {
@@ -47,12 +47,12 @@ public abstract class CyclesModEngine implements NumeralSystemProvider {
 		return applied;
 	}
 
-	private boolean applyPowerProperty(final String key, final String value) throws InvalidNumberException, ValueOutOfRangeException, UnknownPropertyException {
+	private boolean applyPowerProperty(final String propertyName, final String value) throws InvalidNumberException, ValueOutOfRangeException, UnknownPropertyException {
 		boolean applied = false;
-		final short newValue = Power.parse(key, value, numeralSystem.getRadix());
+		final short newValue = Power.parse(propertyName, value, numeralSystem.getRadix());
 
-		final Bike bike = getBike(key);
-		final String suffix = StringUtils.substringAfter(key, Power.PREFIX + '.');
+		final Bike bike = getBike(propertyName);
+		final String suffix = StringUtils.substringAfter(propertyName, Power.PREFIX + '.');
 		if (StringUtils.isNotEmpty(suffix) && StringUtils.isNumeric(suffix) && Integer.parseInt(suffix) < bike.getPower().getCurve().length) {
 			final int index = Integer.parseInt(suffix);
 			final short defaultValue = bike.getPower().getCurve()[index];
@@ -62,17 +62,17 @@ public abstract class CyclesModEngine implements NumeralSystemProvider {
 			}
 		}
 		else {
-			throw new UnknownPropertyException(key);
+			throw new UnknownPropertyException(propertyName);
 		}
 		return applied;
 	}
 
-	private boolean applyGearboxProperty(final String key, final String value) throws ValueOutOfRangeException, InvalidNumberException, UnknownPropertyException {
+	private boolean applyGearboxProperty(final String propertyName, final String value) throws ValueOutOfRangeException, InvalidNumberException, UnknownPropertyException {
 		boolean applied = false;
-		final int newValue = Gearbox.parse(key, value, numeralSystem.getRadix());
+		final int newValue = Gearbox.parse(propertyName, value, numeralSystem.getRadix());
 
-		final Bike bike = getBike(key);
-		final String suffix = StringUtils.substringAfter(key, Gearbox.PREFIX + '.');
+		final Bike bike = getBike(propertyName);
+		final String suffix = StringUtils.substringAfter(propertyName, Gearbox.PREFIX + '.');
 		if (StringUtils.isNotEmpty(suffix) && StringUtils.isNumeric(suffix) && Integer.parseInt(suffix) < bike.getGearbox().getRatios().length) {
 			final int index = Integer.parseInt(suffix);
 			final int defaultValue = bike.getGearbox().getRatios()[index];
@@ -82,17 +82,17 @@ public abstract class CyclesModEngine implements NumeralSystemProvider {
 			}
 		}
 		else {
-			throw new UnknownPropertyException(key);
+			throw new UnknownPropertyException(propertyName);
 		}
 		return applied;
 	}
 
-	private boolean applySettingProperty(final String key, final String value) throws ValueOutOfRangeException, InvalidNumberException, UnknownPropertyException {
+	private boolean applySettingProperty(final String propertyName, final String value) throws ValueOutOfRangeException, InvalidNumberException, UnknownPropertyException {
 		boolean applied = false;
-		final int newValue = Settings.parse(key, value, numeralSystem.getRadix());
+		final int newValue = Settings.parse(propertyName, value, numeralSystem.getRadix());
 
-		final Bike bike = getBike(key);
-		final String suffix = StringUtils.substringAfter(key, Settings.PREFIX + '.');
+		final Bike bike = getBike(propertyName);
+		final String suffix = StringUtils.substringAfter(propertyName, Settings.PREFIX + '.');
 		final Setting setting = Setting.forKey(suffix);
 		if (setting != null) {
 			final int defaultValue = bike.getSettings().getValues().get(setting);
@@ -102,16 +102,16 @@ public abstract class CyclesModEngine implements NumeralSystemProvider {
 			}
 		}
 		else {
-			throw new UnknownPropertyException(key);
+			throw new UnknownPropertyException(propertyName);
 		}
 		return applied;
 	}
 
-	private Bike getBike(final String key) throws UnknownPropertyException {
-		final int displacement = Integer.parseInt(StringUtils.substringBefore(key, "."));
+	private Bike getBike(final String propertyName) throws UnknownPropertyException {
+		final int displacement = Integer.parseInt(StringUtils.substringBefore(propertyName, "."));
 		final BikeType bikeType = BikeType.forDisplacement(displacement);
 		if (bikeType == null) {
-			throw new UnknownPropertyException(key);
+			throw new UnknownPropertyException(propertyName);
 		}
 		return bikesInf.getBikeMap().get(bikeType);
 	}
@@ -126,16 +126,16 @@ public abstract class CyclesModEngine implements NumeralSystemProvider {
 		}
 	}
 
-	public static boolean isPowerProperty(final String key) {
-		return StringUtils.substringAfter(key, ".").startsWith(Power.PREFIX);
+	public static boolean isPowerProperty(final String propertyName) {
+		return StringUtils.substringAfter(propertyName, ".").startsWith(Power.PREFIX);
 	}
 
-	public static boolean isGearboxProperty(final String key) {
-		return StringUtils.substringAfter(key, ".").startsWith(Gearbox.PREFIX);
+	public static boolean isGearboxProperty(final String propertyName) {
+		return StringUtils.substringAfter(propertyName, ".").startsWith(Gearbox.PREFIX);
 	}
 
-	public static boolean isSettingsProperty(final String key) {
-		return StringUtils.substringAfter(key, ".").startsWith(Settings.PREFIX);
+	public static boolean isSettingsProperty(final String propertyName) {
+		return StringUtils.substringAfter(propertyName, ".").startsWith(Settings.PREFIX);
 	}
 
 }
