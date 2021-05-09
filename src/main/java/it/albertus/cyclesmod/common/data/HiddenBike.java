0db2d0605e1c65af5c018022ccf11026fab86c58
Alberto <albertus82@users.noreply.github.com>
2021-05-09 23:51:47 +0200

    HiddenBike

--- src/main/java/it/albertus/cyclesmod/common/data/HiddenBike.java
+++ src/main/java/it/albertus/cyclesmod/common/data/HiddenBike.java
@@ -1,75 +1,55 @@
 package it.albertus.cyclesmod.common.data;
 
+import java.util.Base64;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
+import java.util.zip.DataFormatException;
+import java.util.zip.Inflater;
+
+import it.albertus.cyclesmod.common.model.BikesInf;
+import it.albertus.cyclesmod.common.resources.CommonMessages;
+import it.albertus.cyclesmod.common.resources.Messages;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class HiddenBike {
 
-	private static final long CRC32 = 0xEF30BAE6;
+	private static final long CRC32 = 0xEF30BAE6L;
 
 	private static final String DEFLATE_BASE64 = "eNpjY5DQ22DGr57GnMb8jukd0ynmU8zvpBkYGDoYihiiGDwYnBjsNFCBkY0bduCNDYRCQVQ8DKQgAIgbExMVGgBWamOkzM8KtBoAC/woBQ==";
 
-	//	public static Properties getProperties(@NonNull final BikeType target) {
-	//		final Properties internal = new Properties();
-	//		try (final InputStream is = HiddenBike.class.getResourceAsStream(RESOURCE_NAME)) {
-	//			if (is != null) {
-	//				internal.load(is);
-	//			}
-	//			else {
-	//				throw new FileNotFoundException(RESOURCE_NAME);
-	//			}
-	//		}
-	//		catch (final IOException e) {
-	//			throw new UncheckedIOException("Cannot load resource " + '/' + HiddenBike.class.getPackage().getName().replace('.', '/') + RESOURCE_NAME, e);
-	//		}
-	//
-	//		final Properties properties = new Properties();
-	//
-	//		final String[] settings = internal.getProperty(Settings.PREFIX).split(",");
-	//		if (settings.length != Settings.LENGTH / 2) {
-	//			throw new VerifyError("Invalid " + Settings.PREFIX + " array length, expected " + Settings.LENGTH / 2 + " but was " + settings.length);
-	//		}
-	//		for (final Setting setting : Setting.values()) {
-	//			properties.setProperty(target.getDisplacement() + "." + Settings.PREFIX + "." + setting.getKey(), settings[setting.getIndex()]);
-	//		}
-	//
-	//		final String[] gearbox = internal.getProperty(Gearbox.PREFIX).split(",");
-	//		if (gearbox.length != Gearbox.LENGTH / 2) {
-	//			throw new VerifyError("Invalid " + Gearbox.PREFIX + " array length, expected " + Gearbox.LENGTH / 2 + " but was " + gearbox.length);
-	//		}
-	//		for (int i = 0; i < gearbox.length; i++) {
-	//			properties.setProperty(target.getDisplacement() + "." + Gearbox.PREFIX + "." + i, gearbox[i]);
-	//		}
-	//
-	//		final String[] power = internal.getProperty(Power.PREFIX).split(",");
-	//		if (power.length != Power.LENGTH) {
-	//			throw new VerifyError("Invalid " + Power.PREFIX + " array length, expected " + Power.LENGTH + " but was " + power.length);
-	//		}
-	//		for (int i = 0; i < power.length; i++) {
-	//			properties.setProperty(target.getDisplacement() + "." + Power.PREFIX + "." + i, power[i]);
-	//		}
-	//
-	//		return properties;
-	//	}
-	//
-	//	public static void main(String[] args) throws IOException {
-	//		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	//		try (final InputStream is = Files.newInputStream(Paths.get("d:\\hidden.inf"))) {// HiddenBike.class.getResourceAsStream("HIDDEN.INF")) {
-	//			IOUtils.copy(is, baos, 256);
-	//		}
-	//		System.out.println(baos.toByteArray().length);
-	//		Deflater d = new Deflater(9);
-	//		System.out.println(d.needsInput());
-	//		d.setInput(baos.toByteArray());
-	//		d.finish();
-	//		System.out.println(d.needsInput());
-	//		byte[] buf = new byte[150];
-	//		final int len = d.deflate(buf);
-	//		System.out.println(len);
-	//		byte[] out = Arrays.copyOf(buf, 79);
-	//		System.out.println(out.length);
-	//		System.out.println(Base64.getEncoder().encodeToString(out));
-	//	}
+	private static final Messages messages = CommonMessages.INSTANCE;
+
+	public static byte[] getByteArray() {
+		final int expectedSize = BikesInf.FILE_SIZE / 3;
+		final Inflater inflater = new Inflater();
+		inflater.setInput(Base64.getDecoder().decode(DEFLATE_BASE64));
+		final byte[] bytes = new byte[expectedSize];
+		try {
+			final int actualSize = inflater.inflate(bytes);
+			if (actualSize != expectedSize) {
+				throw new VerifyError(messages.get("common.error.hidden.cfg.corrupted.size", expectedSize, actualSize), new InvalidSizeException(expectedSize, actualSize));
+			}
+		}
+		catch (final DataFormatException e) {
+			throw new VerifyError(messages.get("common.error.hidden.cfg.corrupted"), e);
+		}
+		finally {
+			inflater.end();
+		}
+		final long crc32 = computeCrc32(bytes);
+		if (crc32 != CRC32) {
+			throw new VerifyError(messages.get("common.error.hidden.cfg.corrupted.crc", String.format("%08X", CRC32), String.format("%08X", crc32)));
+		}
+		return bytes;
+	}
+
+	private static long computeCrc32(@NonNull final byte[] bytes) {
+		final Checksum crc = new CRC32();
+		crc.update(bytes, 0, bytes.length);
+		return crc.getValue();
+	}
 
 }
