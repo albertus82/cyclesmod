9a1cb2115d5af1086f27b01405b40f6d75d43a65
Alberto <albertus82@users.noreply.github.com>
2021-05-22 21:44:58 +0200

    Comments.

--- src/main/java/it/albertus/cyclesmod/common/model/Setting.java
+++ src/main/java/it/albertus/cyclesmod/common/model/Setting.java
@@ -12,11 +12,11 @@ public enum Setting { // The order matters.
 	RPM_LIMIT(2, "rpmLimit", 768, 14335, true), // 26239
 	OVERREV_TOLERANCE(3, "overrevTolerance", 0, 32767, true),
 	GRIP(4, "grip", 0, 65535, true),
-	GRIP_0(5, "grip0", 0, 65535, false),
+	GRIP_0(5, "grip0", 0, 65535, false), // unused (pit stop)
 	BRAKING_SPEED(6, "brakingSpeed", 0, 65535, true),
-	BRAKING_SPEED_0(7, "brakingSpeed0", 0, 65535, false),
+	BRAKING_SPEED_0(7, "brakingSpeed0", 0, 65535, false), // unused (pit stop)
 	SPIN_THRESHOLD(8, "spinThreshold", 0, 32767, true),
-	SPIN_THRESHOLD_0(9, "spinThreshold0", 0, 32767, false),
+	SPIN_THRESHOLD_0(9, "spinThreshold0", 0, 32767, false), // unused (pit stop)
 	RPM_DOWNSHIFT(10, "rpmDownshift", 0, 32767, true);
 
 	private final int index;
