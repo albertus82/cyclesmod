9af2f54cc821409a059add0e4b02ebce6a204e70
Alberto <albertus82@users.noreply.github.com>
2021-05-30 16:25:11 +0200

    Test.

---
+++ src/test/java/it/albertus/unexepack/UnExepackTest.java
@@ -0,0 +1,25 @@
+package it.albertus.unexepack;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnExepackTest {
+
+	@Test
+	public void testDecodeExeLen() {
+		Assert.assertEquals(0, UnExepack.decodeExeLen(0, 0));
+		Assert.assertEquals(1, UnExepack.decodeExeLen(1, 1));
+		Assert.assertEquals(511, UnExepack.decodeExeLen(511, 1));
+		Assert.assertEquals(512, UnExepack.decodeExeLen(0, 1));
+		Assert.assertEquals(513, UnExepack.decodeExeLen(1, 2));
+		Assert.assertEquals(0xffff * 512 - 1, UnExepack.decodeExeLen(511, 0xffff));
+		Assert.assertEquals(0xffff * 512, UnExepack.decodeExeLen(0, 0xffff));
+
+		// When e_cp == 0, e_cblp must be 0, otherwise it would encode a negative length.
+		Assert.assertEquals(-1, UnExepack.decodeExeLen(1, 0));
+		Assert.assertEquals(-1, UnExepack.decodeExeLen(511, 0));
+		// e_cblp must be <= 511.
+		Assert.assertEquals(-1, UnExepack.decodeExeLen(512, 1));
+	}
+
+}
