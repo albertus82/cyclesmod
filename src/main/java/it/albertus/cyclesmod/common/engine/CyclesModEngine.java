9121b587cf4ebd334bb83ed12a0d8f1d90704ec5
Alberto <albertus82@users.noreply.github.com>
2021-05-31 08:23:16 +0200

    Refactoring.

--- src/main/java/it/albertus/cyclesmod/common/engine/CyclesModEngine.java
+++ src/main/java/it/albertus/cyclesmod/common/engine/CyclesModEngine.java
@@ -5,7 +5,7 @@ import java.util.logging.Level;
 
 import it.albertus.cyclesmod.common.model.Bike;
 import it.albertus.cyclesmod.common.model.BikeType;
-import it.albertus.cyclesmod.common.model.BikesInf;
+import it.albertus.cyclesmod.common.model.Inf;
 import it.albertus.cyclesmod.common.model.Gearbox;
 import it.albertus.cyclesmod.common.model.Power;
 import it.albertus.cyclesmod.common.model.Setting;
@@ -24,9 +24,9 @@ public class CyclesModEngine {
 
 	private NumeralSystem numeralSystem = NumeralSystem.DEFAULT;
 
-	private BikesInf bikesInf;
+	private Inf bikesInf;
 
-	public CyclesModEngine(final BikesInf bikesInf) {
+	public CyclesModEngine(final Inf bikesInf) {
 		this.bikesInf = bikesInf;
 	}
 
