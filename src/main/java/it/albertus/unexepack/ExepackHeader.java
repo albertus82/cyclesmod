da69d625141819d8c956658b96b000e76b2944da
Alberto <albertus82@users.noreply.github.com>
2021-06-01 07:09:17 +0200

    Refactoring.

---
+++ src/main/java/it/albertus/unexepack/ExepackHeader.java
@@ -0,0 +1,58 @@
+package it.albertus.unexepack;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import lombok.NonNull;
+import lombok.Value;
+
+@Value
+class ExepackHeader {
+
+	static final int SIGNATURE = 0x4252;
+	static final int SIZE = 9 * Short.BYTES; // bytes
+
+	int realIp;
+	int realCs;
+	int memStart; // temporary storage used by the decompression stub
+	int exepackSize; // size of the entire EXEPACK block: header, stub, and packed relocation table
+	int realSp;
+	int realSs;
+	int destLen; // size (in 16-byte paragraphs) of the uncompressed data
+	int skipLen;
+	int signature;
+
+	ExepackHeader(@NonNull final byte[] bytes) throws InvalidExepackHeaderException {
+		if (bytes.length != SIZE) {
+			throw new IllegalArgumentException("Invalid byte array size; expected: " + SIZE + " but was: " + bytes.length);
+		}
+		final ByteBuffer buf = ByteBuffer.wrap(bytes);
+		buf.order(ByteOrder.LITTLE_ENDIAN);
+		realIp = Short.toUnsignedInt(buf.getShort(0));
+		realCs = Short.toUnsignedInt(buf.getShort(2));
+		memStart = Short.toUnsignedInt(buf.getShort(4));
+		exepackSize = Short.toUnsignedInt(buf.getShort(6));
+		realSp = Short.toUnsignedInt(buf.getShort(8));
+		realSs = Short.toUnsignedInt(buf.getShort(10));
+		destLen = Short.toUnsignedInt(buf.getShort(12));
+		final int word8 = Short.toUnsignedInt(buf.getShort(14));
+		final int word9 = Short.toUnsignedInt(buf.getShort(16));
+		if ((word9 != SIGNATURE && word8 != SIGNATURE) || exepackSize == 0x00) {
+			throw new InvalidExepackHeaderException(bytes);
+		}
+		if (word8 == SIGNATURE && word9 != SIGNATURE) {
+			this.skipLen = 1;
+			this.signature = word8;
+		}
+		else {
+			this.skipLen = word8;
+			this.signature = word9;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return String.format("realIp = 0x%04X, realCs = 0x%04X, memStart = 0x%04X, exepackSize = 0x%04X, realSp = 0x%04X, realSs = 0x%04X, destLen = 0x%04X, skipLen = 0x%04X, signature = 0x%04X", realIp, realCs, memStart, exepackSize, realSp, realSs, destLen, skipLen, signature);
+	}
+
+}
