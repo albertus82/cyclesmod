fee7b1d8b98050e9bf75c0684d854c9d25456173
Alberto <albertus82@users.noreply.github.com>
2021-06-20 11:46:03 +0200

    Added unexepack executable.

---
+++ src/main/bin/linux/unexepack.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+PRG="$0"
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+PRGDIR=`dirname "$PRG"`
+java -Xms${unexepack.vm.initialHeapSize}m -Xmx${unexepack.vm.maxHeapSize}m -cp "$PRGDIR/${project.build.finalName}.${project.packaging}:$PRGDIR/lib/*.jar" ${unexepack.mainClass} "$@"
