b4761d59fb53235038f630304eede48f2748a64d
Alberto <albertus82@users.noreply.github.com>
2026-08-20 21:27:24 +0200

    Human readable torque/power values

--- src/main/java/io/github/albertus82/cyclesmod/gui/torquegraph/dialog/listener/UpdateTitleListener.java
+++ src/main/java/io/github/albertus82/cyclesmod/gui/torquegraph/dialog/listener/UpdateTitleListener.java
@@ -10,22 +10,16 @@ import io.github.albertus82.cyclesmod.common.model.Torque;
 import io.github.albertus82.cyclesmod.common.resources.ConfigurableMessages;
 import io.github.albertus82.cyclesmod.gui.Mode;
 import io.github.albertus82.cyclesmod.gui.resources.GuiMessages;
-import io.github.albertus82.cyclesmod.gui.torquegraph.BasicTorqueGraph;
 import io.github.albertus82.cyclesmod.gui.torquegraph.TorqueGraph;
 import lombok.NonNull;
 
 public class UpdateTitleListener extends MouseMotionListener.Stub {
 
-	//	private static final double NM_TO_LBFT = 0.73756214927727;
-	//	private static final double KW_TO_HP = 1.3404825737265;
-	//
-	//private static final float GPC_FACTOR = 6.8f; // Ferrari F1/87/88C (Ferrari 3.5L V12 - 680 bhp (507 kW; 689 PS))
+	private static final double NM_TO_LBFT = 0.73756214927727;
+	private static final double KW_TO_HP = 1.3404825737265;
 
-	//private static final float CYCLES_POWER_FACTOR = 9.2f;
 	private static final float CYCLES_TORQUE_FACTOR = 0.45f;
-
-	//private static final float GPC_POWER_FACTOR = 9.2f;
-	private static final float GPC_TORQUE_FACTOR = 3.3f;
+	private static final float GPC_TORQUE_FACTOR = 3.4f;
 
 	private static final ConfigurableMessages messages = GuiMessages.INSTANCE;
 
@@ -61,9 +55,9 @@ public class UpdateTitleListener extends MouseMotionListener.Stub {
 	private void handleEvent(final Point location) {
 		final short torque = torqueGraph.getTorqueValue(location);
 		final int rpm = Torque.getRpm(torqueGraph.getTorqueIndex(location));
-		final double power = BasicTorqueGraph.torqueToPower(torque, rpm);
-		// TODO convert to human readable measure units
-		final String currentPosition = messages.get("gui.label.graph.torqueAtRpm", torque, numberFormat.format(power), rpm);
+		final double torqueLbFt = torque * (Mode.CYCLES.equals(mode) ? CYCLES_TORQUE_FACTOR : GPC_TORQUE_FACTOR);
+		final double powerHp = (torqueLbFt * rpm) / 5252;
+		final String currentPosition = messages.get("gui.label.graph.torqueAtRpm", torque, numberFormat.format(torqueLbFt), numberFormat.format(torqueLbFt / NM_TO_LBFT), numberFormat.format(powerHp), numberFormat.format(powerHp / KW_TO_HP), rpm);
 		if (!currentPosition.equals(lastPosition)) {
 			lastPosition = currentPosition;
 			torqueGraph.getXyGraph().setTitle(lastPosition);
